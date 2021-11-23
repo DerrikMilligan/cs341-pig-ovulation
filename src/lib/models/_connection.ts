a0e2c1b3b89dfaeca78d9a554134e0258a8ace90
Derrik Milligan <derrik.milligan.dev@gmail.com>
2021-11-22 18:12:11 -0700

    Changed some stuff for Heroku to build correctly.

--- src/lib/models/_connection.ts
+++ src/lib/models/_connection.ts
@@ -1,22 +1,27 @@
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 
-// Make sure that our .env file has been loaded
-const envResult = dotenv.config();
-if (envResult.error) {
-  throw envResult.error;
-}
-
-// For some reason when trying to access env variables with process.env.MONGO_USERNAME it doesn't work...
-// We can access them with keys... Like process.env['MONGO_USERNAME'] but that's pretty ugly...
-// So for now we'll just use the parsed data from the parsed config file
-const env = envResult.parsed;
+let MONGODB_URI = '';
 
 // Load the URI from Heroku if theres one in the environment
+if (process.env.MONGODB_URI) {
+  MONGODB_URI = process.env.MONGODB_URI;
+
 // otherwise build the URI
-const MONGODB_URI = 
-  process.env.MONGODB_URI ||
-  `mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@main.mlsay.mongodb.net/${env.MONGO_DATABASE}?retryWrites=true&w=majority`;
+} else {
+  // Make sure that our .env file has been loaded
+  const envResult = dotenv.config();
+  if (envResult.error) {
+    throw envResult.error;
+  }
+
+  // For some reason when trying to access env variables with process.env.MONGO_USERNAME it doesn't work...
+  // We can access them with keys... Like process.env['MONGO_USERNAME'] but that's pretty ugly...
+  // So for now we'll just use the parsed data from the parsed config file
+  const env = envResult.parsed;
+
+  MONGODB_URI = `mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@main.mlsay.mongodb.net/${env.MONGO_DATABASE}?retryWrites=true&w=majority`;
+}
 
 /**
  * Global is used here to maintain a cached connection across hot reloads
