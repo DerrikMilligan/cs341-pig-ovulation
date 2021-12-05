e4a51386ea1ebf4b78b278ce6e6656fcf79969fe
Tyler Lusk <87098799+tee-lusk@users.noreply.github.com>
2021-12-04 22:08:03 -0700

    Changes from master

--- src/routes/farms/[id].svelte
+++ src/routes/farms/[id].svelte
@@ -9,8 +9,8 @@
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify({
-				farmId: _id
-			}, null, 2)
+				farmId: _id,
+			}, null, 2),
 		});
 
 		let props = {
@@ -48,13 +48,50 @@
 
 <script lang='ts'>
 	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
 	import type { Pig } from '$lib/types';
 	import type { User } from '$lib/types';
 	import PigList from '$lib/components/PigList.svelte';
+	import Modal from '$lib/components/Modal.svelte';
 	import UserList from '$lib/components/UserList.svelte';
 
 	export let pigs: Pig[];
+	let show = false;
+	let pigId;
+	let errorMessage = '';
 	export let users: User[];
+
+	function showModal(event) {
+		console.log(`delete modal for pig: ${event.detail.pigId}`);
+		pigId = event.detail.pigId;
+		show = true;
+	}
+
+	async function deletePig(pigId) {
+		const res = await fetch(
+			'/pigs/deletePig',
+			{
+				method: 'POST',
+				body: JSON.stringify({
+					_id: pigId
+				}),
+			},
+		);
+
+		console.log(res);
+
+		show = false;
+
+		if (res.ok) {
+			const body = await res.json();
+			console.log(body);
+			pigs = pigs.filter((pig) => pig._id !== pigId);
+		} else {
+			const body = await res.json();
+			console.log('ERROR!');
+			errorMessage = body.message;
+		}
+	}
 </script>
 
 <svelte:head>
@@ -65,7 +102,18 @@
 
 <a class='btn btn-primary btn-sm p-1 mx-auto my-3' href='/pigs/new/{$page.params.id}'>Add New Pig</a>
 
-<PigList {pigs} />
+<PigList {pigs} on:deleteClicked={showModal}/>
+
+{#if show}
+	<Modal>
+		<h1 slot="header">Deleting Pig</h1>
+		<div slot="content" class="pt-4">
+			<p>Are you sure you would like to delete you pig?</p>
+			<button class="btn btn-primary btn-sm p-1" on:click={(e) => {e.preventDefault(); show = false;}}>Cancel</button>
+			<button class="btn btn-primary btn-sm p-1" on:click={() => deletePig(pigId)}>Delete Pig</button>
+		</div>
+	</Modal>
+{/if}
 
 <UserList {users} />
 
